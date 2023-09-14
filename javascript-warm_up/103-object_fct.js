02045230ff00541e275fa2d13ce1b259b0153361
mikemanuu <mikemanuu273@gmail.com>
2023-09-14 16:39:10 +0300

    task 8

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -4,15 +4,14 @@ const myObject = {
     type: 'object',
     value: 12
 };
-
 console.log(myObject);
 myObject.incr = function () {
     this.value++;
 };
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
 
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
